d00ba7eae7dec5d2ff93b722424591cf59055b2b
Jonas Stenberg <github@stenberg.io>
2026-02-22 12:30:11 +0100

    Fix migration V48: skip if backupuser role doesn't exist (CI)

--- flyway/sql/V48__backupuser_default_privileges.sql
+++ flyway/sql/V48__backupuser_default_privileges.sql
@@ -1,5 +1,10 @@
 -- Grant SELECT on tables that were missed when backupuser was created
-GRANT SELECT ON ai_review_runs, ai_review_suggestions TO backupuser;
-
--- Ensure backupuser automatically gets SELECT on any future tables
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO backupuser;
+-- Wrapped in DO block so it's a no-op in environments without backupuser (CI)
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'backupuser') THEN
+    EXECUTE 'GRANT SELECT ON ai_review_runs, ai_review_suggestions TO backupuser';
+    EXECUTE 'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO backupuser';
+  END IF;
+END
+$$;
